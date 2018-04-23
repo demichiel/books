87c4c383abf2351cf2768dcf89378bb0b91fd610
Michiel R <demichiel@gmail.com>
2018-04-23 15:56:04 +0200

    add Vivus dependency and add SVG logo

--- src/components/App.vue
+++ src/components/App.vue
@@ -3,8 +3,12 @@
     <div class="container">
       <Nav />
       <div class="jumbotron">
+        <div class="row">
+          <div class="col-2"><object id="my-svg" type="image/svg+xml" :data="require('../assets/book.svg')"></object></div>
+          <div class="col">
         <h1 class="display-4">Leeslijst</h1>
-        <p class="lead">A place to keep track of the books I'm reading</p>
+        <p class="lead">A place to keep track of the books I'm reading</p></div>
+        </div>
       </div>
       <router-view />
     </div>
@@ -15,6 +19,7 @@
 import store from "../store"
 import SeriesList from '../components/SeriesList.vue'
 import Nav from '../components/Nav.vue'
+import Vivus from 'Vivus'
 
 export default {
   name: "app",
@@ -26,6 +31,14 @@ export default {
   components : {
     SeriesList,
     Nav,
+  },
+  mounted() {
+    new Vivus('my-svg', {
+    reverseStack: true,
+    type: 'sync',
+    duration: 200,
+    animTimingFunction: Vivus.EASE,
+    })
   }
 }
 </script>
